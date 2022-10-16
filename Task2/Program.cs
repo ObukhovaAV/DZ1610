a9e64f3a62f8101f89af3e898007aecf4fcf867e
Anastasya <okayota83@mail.ru>
2022-10-16 21:03:11 +0400

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void FillMatrix(int[,] matrix,int m,int n)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)      
+            matrix [i,j]= m+n;    
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+Console.WriteLine("Задайте размер массива mxn");
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int [,] matrix = new int [m,n];
+FillMatrix (matrix,  m, n);
+Console.WriteLine("Ваш массив: ");
+PrintMatrix (matrix);
