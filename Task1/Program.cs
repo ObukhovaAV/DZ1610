d8bda439c66ee666cdb14dfce7acf2cf62557b8b
Anastasya <okayota83@mail.ru>
2022-10-17 15:05:03 +0400

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+
+void FillMatrix (double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i,j] = new Random().NextDouble()*100;
+}
+
+
+void PrintMatrix (double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+         for (int j = 0; j < matrix.GetLength(1); j++)
+          {
+           Console.Write($"{matrix[i,j]} ");   
+          }
+         Console.WriteLine();
+    }
+}
+double [,] matrix = new double [3,4];
+FillMatrix (matrix);
+PrintMatrix (matrix);
